0935c909f2e4381261d3841f9dd848bb3392c219
nariana <nariana.pereira@outlook.com>
2024-01-14 20:00:59 -0300

    Creates update endpoint

--- controllers/products.go
+++ controllers/products.go
@@ -2,6 +2,7 @@ package controllers
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 	"strconv"
 	"text/template"
@@ -48,5 +49,33 @@ func Delete(w http.ResponseWriter, r *http.Request) {
 }
 
 func Edit(w http.ResponseWriter, r *http.Request) {
-	temp.ExecuteTemplate(w, "Edit", nil)
+	productId := r.URL.Query().Get("id")
+	product := models.GetProduct(productId)
+	temp.ExecuteTemplate(w, "Edit", product)
+}
+
+func Update(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "POST" {
+		id := r.FormValue("id")
+		name := r.FormValue("name")
+		description := r.FormValue("description")
+		price := r.FormValue("price")
+		quantity := r.FormValue("quantity")
+
+		convertedId, err := strconv.Atoi(id)
+		if err != nil {
+			log.Println("Erro na conversão do Id para int", err.Error())
+		}
+		convertedPrice, err := strconv.ParseFloat(price, 64)
+		if err != nil {
+			fmt.Println(err.Error())
+		}
+		convertedQuantity, err := strconv.Atoi(quantity)
+		if err != nil {
+			fmt.Println(err.Error())
+		}
+		models.Update(convertedId, convertedPrice, convertedQuantity, name, description)
+	}
+	http.Redirect(w, r, "/", 301)
+
 }
--- models/products.go
+++ models/products.go
@@ -1,6 +1,8 @@
 package models
 
 import (
+	"log"
+
 	"github.com/store-web-go/db"
 )
 
@@ -16,7 +18,7 @@ func GetAllProducts() []Product {
 
 	database := db.ConnectDb()
 
-	selectAllProducts, err := database.Query("select * from products")
+	selectAllProducts, err := database.Query("select * from products order by id asc")
 	if err != nil {
 		panic(err.Error())
 	}
@@ -46,6 +48,33 @@ func GetAllProducts() []Product {
 	return products
 }
 
+func GetProduct(idToGet string) Product {
+	db := db.ConnectDb()
+	productData, err := db.Query("select * from products where id=$1", idToGet)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	productReturn := Product{}
+	var id, quantity int
+	var name, description string
+	var price float64
+
+	for productData.Next() {
+		err = productData.Scan(&id, &name, &description, &price, &quantity)
+		if err != nil {
+			panic(err.Error())
+		}
+		productReturn.Id = id
+		productReturn.Name = name
+		productReturn.Description = description
+		productReturn.Price = price
+		productReturn.Quantity = quantity
+	}
+	defer db.Close()
+	return productReturn
+}
+
 func CreateNewProduct(name, description string, convertedPrice float64, convertedQuantity int) {
 	db := db.ConnectDb()
 	insertData, err := db.Prepare("insert into products(name, description, price, quantity) values($1, $2, $3, $4)")
@@ -65,5 +94,17 @@ func Delete(productId string) {
 		panic(err.Error())
 	}
 	deleteData.Exec(productId)
+	defer db.Close()
+}
+
+func Update(id int, price float64, quantity int, name string, description string) {
+	db := db.ConnectDb()
+
+	updateData, err := db.Prepare("update products set name=$1, description=$2, price=$3, quantity=$4 where id=$5")
+	if err != nil {
+		log.Println(err.Error())
+	}
+
+	updateData.Exec(name, description, price, quantity, id)
 	defer db.Close()
 }
